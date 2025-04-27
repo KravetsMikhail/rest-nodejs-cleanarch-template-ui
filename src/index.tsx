56c1adcdeaad231c6d1181e28ce946e14e9f2e1e
Kravets Mikhail <kravetsmihail@gmail.com>
2025-04-27 21:17:48 +0300

    настроен работа с keycloak

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,26 @@ import Keycloak from "keycloak-js";
 import App from "./App";
 
 const keycloak = new Keycloak({
-  clientId: "refine-demo",
-  url: "https://lemur-0.cloud-iam.com/auth",
-  realm: "refine",
+  clientId: "rest-nodejs-cleanarch-template-ui",
+  url: "http://localhost:8282",
+  realm: "rest-nodejs-cleanarch-template"
 });
+
+const initOptions = {
+  onLoad: 'login-required',
+  checkLoginIframe: false,
+  pkceMethod: 'S256',
+  flow: 'standard',
+  enableLogging: true,
+  silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+  redirectUri: window.location.origin
+};
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-  <ReactKeycloakProvider authClient={keycloak}>
+  <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
     <App />
   </ReactKeycloakProvider>
 );
